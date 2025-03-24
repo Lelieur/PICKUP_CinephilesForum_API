eb19b82a894a5c53e69110a44c4e8389a2a6849b
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2025-03-24 20:02:01 +0100

    CORS corrected

--- config/index.js
+++ config/index.js
@@ -3,20 +3,20 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || 'https://lareview2.netlify.app';
+const FRONTEND_URL = process.env.ORIGIN || "https://pickup-blush.vercel.app";
 
 module.exports = (app) => {
-  app.set("trust proxy", 1)
+  app.set("trust proxy", 1);
 
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL],
     })
-  )
+  );
 
-  app.use(logger("dev"))
+  app.use(logger("dev"));
 
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
-  app.use(cookieParser())
-}
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
